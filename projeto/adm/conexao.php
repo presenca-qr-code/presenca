fe04d4222661fdae996ff177909a04e8f9352423
Ryan <ryan.noviski@alunos.sc.senac.br>
2022-05-02 20:31:20 -0300

    ajusta pastas

--- projeto/adm/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-$local="localhost";
-$user="root";
-$senha="";
-$banco="qrcode";
-$conectar = mysqli_connect($local,$user,$senha) or die ("Erro na conexão");
-mysqli_select_db($conectar, $banco)or die ("Base não encontrada");
-?>
--- projeto/adm/usuario.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-include_once("seguranca.php");
-echo "Bem vindo usuário ".$_SESSION['usuarioNome'];
-?>
-<br />
-<a href="sair.php">Sair</a>
